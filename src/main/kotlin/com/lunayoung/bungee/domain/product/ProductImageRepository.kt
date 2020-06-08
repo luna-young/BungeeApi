7583838e4264dc8cbdf9865334a470e5a70210c8
Soyoung Luna Yang <luna.ensocorro@gmail.com>
2020-06-08 13:37:34 +0900

    상품정보와 상품이미지 정보를 저장하기 위한 레파지토리

---
+++ src/main/kotlin/com/lunayoung/bungee/domain/product/ProductImageRepository.kt
@@ -0,0 +1,8 @@
+package com.lunayoung.bungee.domain.product
+
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface ProductImageRepository : JpaRepository<ProductImage, Long> {
+
+    fun findByIdIn(imageIds: List<Long>): MutableList<ProductImage>
+}
---
+++ src/main/kotlin/com/lunayoung/bungee/domain/product/ProductRepository.kt
@@ -0,0 +1,8 @@
+package com.lunayoung.bungee.domain.product
+
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface ProductRepository : JpaRepository<Product, Long>{
+
+
+}
